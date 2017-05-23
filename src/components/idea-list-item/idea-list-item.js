b9e908b50ae2005436654e96593d20464108cf57
raffaeleabramini <raffaele.abramini@gmail.com>
2017-05-23 16:47:39 +0100

    Add item list style

--- src/components/idea-list-item/idea-list-item.js
+++ src/components/idea-list-item/idea-list-item.js
@@ -3,19 +3,34 @@ import PropTypes from 'prop-types';
 import cl from 'classnames';
 import {Link} from 'react-router-dom';
 import config from '../../config';
+import ideaListItem from './idea-list-item.scss';
 
 const IdeaListItem = ({isCompleted, title, id, handleUpdateClick, handleDeleteClick})=>{
 	let classes =  cl({
-		'idea' : true,
-		'is-completed' : isCompleted
+		[ideaListItem.item]	 : true,
+		[ideaListItem.isCompleted] : isCompleted
 	});
 	return (
 		<li
 			className={classes}>
-			<Link to={config.routes.viewIdea(id)}>{title}</Link>
+			<Link
+				className={ideaListItem.title}
+				to={config.routes.viewIdea(id)}>
+				{title}
+			</Link>
 
-			<button onClick={handleUpdateClick.bind(this, id, isCompleted)}>toggle complete</button>
-			<button onClick={handleDeleteClick.bind(this, id)}>delete</button>
+			<div className={ideaListItem.buttons}>
+				<button
+					className={cl(ideaListItem.button, ideaListItem.first)}
+					onClick={handleUpdateClick.bind(this, id, isCompleted)}>
+					toggle complete
+				</button>
+				<button
+					className={cl(ideaListItem.button, ideaListItem.second)}
+					onClick={handleDeleteClick.bind(this, id)}>
+					delete
+				</button>
+			</div>
 		</li>
 	)
 };
