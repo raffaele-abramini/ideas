81c727ada003070fe35746a1e149525cad368187
raffaeleabramini <raffaele.abramini@gmail.com>
2017-05-24 13:36:56 +0100

    Idea form styles

--- src/views/idea-form.js
+++ src/views/idea-form.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import cl from 'classnames';
 
 import { Field, FieldArray, reduxForm } from 'redux-form';
 import { Redirect } from 'react-router';
+import field from '../styles/_field.scss';
+import button from '../styles/_button.scss';
 import overlay from '../components/overlay/overlay';
 
-
-const SubFieldsHolder = ({fields=[]}) => {
-
-	return <div className="subFields">
-		<button type="button" onClick={() => {
-			fields.push({});
-		}}>Add section</button>
-
-		{fields.map((section, index) => {
-			return	<div key={index}>
-				<Field
-					component='input'
-					type="text"
-					name={`${section}.title`}
-					placeholder="insert title"
-					validate={[value => value && value.length > 3 ? undefined : 'Too short']}
-				/>
-				<Field
-					component='textarea'
-					name={`${section}.content`}
-					placeholder="insert content"
-					validate={[value => value && value.length > 3 ? undefined : 'Too short']}
-				/>
-			</div>
-		})}
-	</div>
-};
 
 class IdeaForm extends Component {
 	static propTypes = {
@@ -59,38 +35,54 @@ class IdeaForm extends Component {
 
 		const {formTitle} = this.props;
 
-		return <form onSubmit={this.props.handleSubmit(this.handleSubmit)}>
-			<h4>{formTitle}</h4>
-			<div>
-				<Field
-					component='input'
-					type="text"
-					name="title"
-					placeholder="insert title"
-					validate={[value => value && value.length > 3 ? undefined : 'Too short']}
-					ref={node => this.title = node}
-				/>
-			</div>
+		return (
+			<form onSubmit={this.props.handleSubmit(this.handleSubmit)}>
+				<h4 className={field.title}>{formTitle}</h4>
+				<div
+					className={field.row}>
+					<Field
+						component='input'
+						type="text"
+						name="title"
+						className={cl(field.input, field.inputTitle)}
+						placeholder="Title here"
+						validate={[value => value && value.length > 3 ? undefined : 'Too short']}
+						ref={node => this.title = node}
+					/>
+				</div>
 
-			<div>
-				<Field
-					component='textarea'
-					name="content"
-					placeholder="insert content"
-					validate={[value => value && value.length > 3 ? undefined : 'Too short']}
-					ref={node => this.content = node}
-				/>
-			</div>
+				<div
+					className={field.row}>
+					<Field
+						component='textarea'
+						name="content"
+						className={field.textarea}
+						placeholder="Content here"
+						validate={[value => value && value.length > 3 ? undefined : 'Too short']}
+						ref={node => this.content = node}
+					/>
+				</div>
 
-			<div>
-				<h5>Sections</h5>
-				<FieldArray name="sections"
-							component={SubFieldsHolder}>
-				</FieldArray>
-			</div>
+				<div
+					className={field.section}>
+					<h5 className={field.subtitle}>Sections</h5>
+					<FieldArray name="sections"
+								component={SubFieldsHolder}
+								className={field.row}>
+					</FieldArray>
+				</div>
 
-			<button type="submit" disabled={this.props.pristine || !this.props.valid}>Submit</button>
-		</form>
+				<div
+					className={field.section}>
+					<button
+						type="submit"
+						disabled={this.props.pristine || !this.props.valid}
+						className={button.button}>
+						Submit
+					</button>
+				</div>
+			</form>
+		)
 	}
 
 	handleSubmit({title, content, sections=[]}){
@@ -105,5 +97,51 @@ class IdeaForm extends Component {
 		this.setState({formSubmitted:true});
 	}
 }
+
+
+const SubFieldsHolder = ({fields=[]}) => {
+	return (
+		<div className="subFields">
+			{fields.map((section, index) => {
+				return	(
+					<div
+						key={index}
+						className={field.section}>
+						<div
+							className={field.row}>
+							<Field
+								component='input'
+								type="text"
+								name={`${section}.title`}
+								className={field.input}
+								placeholder="Section title here"
+								validate={[value => value && value.length > 3 ? undefined : 'Too short']}
+							/>
+						</div>
+
+						<div
+							className={field.row}>
+							<Field
+								component='textarea'
+								name={`${section}.content`}
+								className={field.textarea}
+								placeholder="Section content here"
+								validate={[value => value && value.length > 3 ? undefined : 'Too short']}
+							/>
+						</div>
+					</div>
+				)
+			})}
+
+			<button
+				type="button"
+				onClick={() => {
+					fields.push({});
+				}}
+				className={button.vanilla}
+			>Add new section</button>
+		</div>
+	)
+};
 
 export default reduxForm({form: 'idea-form'})(overlay(IdeaForm));
