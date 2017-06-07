5fecc32fb55be3a4121419c21169779673630c7a
raffaeleabramini <raffaele.abramini@gmail.com>
2017-06-07 12:58:03 +0100

    Initial setup

--- src/root.js
+++ src/root.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk'
-import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import config from './config'
+
+import AuthController from './containers/auth-controller';
+import Loader from './containers/loader-container';
+import IdeasListView from './containers/ideas-list-container'
+import LoginView from './views/login-view'
+import SignupView from './views/signup-view';
+import WelcomeView from './views/welcome-view';
+import Header from './containers/header-container';
+import layout from './styles/_layout.scss';
+import './styles/_transitions.scss';
+import AuthRoute from './containers/auth-route';
 
 
-import reducer from './reducers';
-import App from './app';
+export default () => {
+	return (
+		<div className={layout.body}>
+			<AuthController/>
+			<Loader/>
+			<Header/>
 
-const store = createStore(reducer, applyMiddleware(thunk));
-
-
-const Root = ()=>{
-	return <Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Provider>
-};
-
-
-ReactDOM.render(<Root/>, document.querySelector('.app'));
+			<div className={layout.container}>
+				<Switch>
+					<Route exact path={config.routes.home} component={WelcomeView}/>
+					<AuthRoute path={config.routes.signup} component={SignupView} isProtected={false}/>
+					<AuthRoute path={config.routes.login} component={LoginView} isProtected={false}/>
+					<AuthRoute path={config.routes.list} component={IdeasListView} isProtected={true}/>
+				</Switch>
+			</div>
+		</div>
+	)
+}
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,41 +2,20 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-
-	entry: path.resolve(__dirname, 'server.js'),
-
-	output: {
-		filename: 'server.bundle.js'
+	resolve: {
+		extensions: [".js", ".jsx"],
+		alias: {
+			'firebase-database': path.resolve(__dirname, '../functions/firebase-database'),
+		},
 	},
-
+	resolveLoader: {
+		modules: [path.resolve(__dirname, "./node_modules")],
+	},
 	target: 'node',
-
-	// keep node_module paths out of the bundle
-	externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
-		'react-dom/server', 'react/addons',
-	]).reduce(function (ext, mod) {
-		ext[mod] = 'commonjs ' + mod
-		return ext
-	}, {}),
-
-	node: {
-		__filename: true,
-		__dirname: true
-	},
-
-	module: {
-		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-2' },
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					use: ["css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]", "sass-loader"]
-				})
-			},
-		]
-	},
-
-	plugins : [
-		new ExtractTextPlugin('./style.css')
-	]
+	entry: './src/server.js',
+	output: {
+		filename: 'server.bundle.js',
+		path: path.resolve(__dirname, '../functions/build'),
+		libraryTarget: 'commonjs2',
+	}
 }
