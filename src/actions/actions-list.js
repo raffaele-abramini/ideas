a47e4325aa5981b9a0f7b0304ffc8ab30ba2e92c
raffaeleabramini <raffaele.abramini@gmail.com>
2017-06-08 11:54:14 +0100

    Use session to store show/hide completed

--- src/actions/actions-list.js
+++ src/actions/actions-list.js
@@ -1,6 +1,10 @@
 export const TOGGLE_COMPLETED_IDEAS = 'TOGGLE_COMPLETED_IDEAS';
 
 export const toggleCompletedIdeas = ()=>{
+	global.localStorage.getItem('hideCompletedIdeas')
+		? global.localStorage.removeItem('hideCompletedIdeas')
+		: global.localStorage.setItem('hideCompletedIdeas', 'true')
+
 	return {
 		type: TOGGLE_COMPLETED_IDEAS
 	}
--- src/reducers/reducer-list.js
+++ src/reducers/reducer-list.js
@@ -1,13 +1,19 @@
 import {TOGGLE_COMPLETED_IDEAS} from '../actions/actions-list';
 
+const getLocalItem = itemName => {
+	if(!global.localStorage) return false;
+
+	return !!global.localStorage.getItem(itemName);
+}
+
 const initialState = {
-	hideCompletedIdeas : false
+	hideCompletedIdeas : getLocalItem('hideCompletedIdeas')
 };
 
 export default (state = initialState, action) => {
 	switch (action.type){
 		case TOGGLE_COMPLETED_IDEAS:
-			return {...state, hideCompletedIdeas: !state.hideCompletedIdeas};
+			return {...state, hideCompletedIdeas: getLocalItem('hideCompletedIdeas')};
 
 		default:
 			return state;
