cc3d479f304ada99b8ae094bba7e92cc1f085671
oldirony <raffaele.abramini@gmail.com>
2018-05-12 13:24:13 +0100

    Single idea
    - add button to toggle 'compact view'

--- src/components/single-idea/single-idea.js
+++ src/components/single-idea/single-idea.js
@@ -1,110 +1,124 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import config from '../../config';
-import cl from 'classnames';
-import {Link} from 'react-router-dom';
-import idea from './single-idea.scss';
-import {getFormattedDate} from '../../lib/utils';
-import button from 'styles/_button.scss';
-import icon from 'styles/_icon.scss';
-import overlay from '../overlay/overlay.scss';
-import Section from '../idea-section/idea-section';
-import {getDateMessageAlert} from '../../lib/utils';
+import React from 'react'
+import PropTypes from 'prop-types'
+import config from '../../config'
+import cl from 'classnames'
+import {Link} from 'react-router-dom'
+import idea from './single-idea.scss'
+import button from 'styles/_button.scss'
+import icon from 'styles/_icon.scss'
+import overlay from '../overlay/overlay.scss'
+import Section from '../idea-section/idea-section'
+import {getDateMessageAlert, getFormattedDate} from '../../lib/utils'
 
 
-const SingleIdea = ({title, timestamp, content, sections, id, updateIdeaSections, coverImage, deadline, isCompleted})=>{
-	const updateSection = (sectionIndex)=>{
-		sections[sectionIndex].isCompleted = !sections[sectionIndex].isCompleted;
-		updateIdeaSections(sections);
-	};
+const SingleIdea = ({
+                      title,
+                      timestamp,
+                      content,
+                      sections,
+                      id,
+                      updateIdeaSections,
+                      coverImage,
+                      deadline,
+                      isCompleted,
+                      toggleCompactIdeaView,
+                      ideaCompactView
+                    }) => {
+  const updateSection = (sectionIndex) => {
+    sections[sectionIndex].isCompleted = !sections[sectionIndex].isCompleted
+    updateIdeaSections(sections)
+  }
 
 
-	const showDeadlineAlert = ()=>{
-		if(!deadline || isCompleted) return '';
+  const showDeadlineAlert = () => {
+    if (!deadline || isCompleted) return ''
 
-		const message = getDateMessageAlert(deadline)
+    const message = getDateMessageAlert(deadline)
 
-		if(message) {
-			return (
-				<div className={cl(
-					idea.expirationMessage,
-					idea[message.code]
-				)}>
-					{message.message}
-				</div>
-			)
-		}
-	}
+    if (message) {
+      return (
+        <div className={cl(
+          idea.expirationMessage,
+          idea[message.code]
+        )}>
+          {message.message}
+        </div>
+      )
+    }
+  }
 
-	return (
-        <div className={cl(idea.idea,{
-        	[idea.withBg] : coverImage
-		})}>
-			{coverImage && (
-				<div className={idea.coverImageContainer} style={{backgroundImage:`url(${coverImage}`}}>
-				</div>
-			)}
+  return (
+    <div className={cl(idea.idea, {
+      [idea.withBg]: coverImage
+    })}>
+      {coverImage && (
+        <div className={idea.coverImageContainer} style={{backgroundImage: `url(${coverImage}`}}>
+        </div>
+      )}
 
-			<div className={idea.titleContainer}>
-				<h2 className={idea.title}>{title}</h2>
-			</div>
+      <div className={idea.titleContainer}>
+        <h2 className={idea.title}>{title}</h2>
+      </div>
 
+      <div className={idea.date}>
+        <time>
+          <svg className={icon.icon}>
+            <use xlinkHref="#calendar"/>
+          </svg>
+          Created: {getFormattedDate(timestamp)}
+        </time>
 
-			<div className={idea.date}>
-				<time>
-					<svg className={icon.icon}>
-						<use xlinkHref="#calendar"/>
-					</svg>
-					Created: {getFormattedDate(timestamp)}
-				</time>
+        {deadline &&
+        <time className={idea.deadline}>
+          <svg className={icon.icon}>
+            <use xlinkHref="#flag"/>
+          </svg>
+          Deadline: {getFormattedDate(deadline)}
+        </time>
+        }
+      </div>
+      {showDeadlineAlert()}
 
-				{deadline &&
-				<time className={idea.deadline}>
-					<svg className={icon.icon}>
-						<use xlinkHref="#flag"/>
-					</svg>
-					Deadline: {getFormattedDate(deadline)}
-				</time>
-				}
-			</div>
+      <nav onClick={() => toggleCompactIdeaView()}>
+        <button className={button.vanilla}>Toggle compact view</button>
+      </nav>
 
-			{showDeadlineAlert()}
+      {idea.content && (
+        <div className={idea.content}>
+          {content.split('\n').map((subcontent, index) => {
+            if (!subcontent) return
+            return <p key={index}>{subcontent}</p>
+          })}
+        </div>
+      )}
 
-			{idea.content && (
-				<div className={idea.content}>
-					{content.split('\n').map((subcontent, index)=> {
-						if(!subcontent) return;
-						return <p key={index}>{subcontent}</p>
-					})}
-				</div>
-			)}
+      <ul className={idea.sections}>
+        {sections && sections.map((section, index) => (
+          <Section key={index}
+                   {...section}
+                   index={index}
+                   ideaCompactView={ideaCompactView}
+                   updateSection={updateSection}/>
+        ))}
+      </ul>
 
-			<ul className={idea.sections}>
-				{sections && sections.map((section,index) => (
-					<Section key={index}
-							 {...section}
-							 index={index}
-							 updateSection={updateSection}/>
-				))}
-			</ul>
+      <nav className={overlay.nav}>
+        <Link
+          className={button.button}
+          to={config.routes.editIdea(id)}>Edit</Link>
 
-			<nav className={overlay.nav}>
-				<Link
-					className={button.button}
-					to={config.routes.editIdea(id)}>Edit</Link>
-
-				<Link
-					className={button.button}
-					to={config.routes.list}>Close</Link>
-			</nav>
-		</div>
-    )
-};
+        <Link
+          className={button.button}
+          to={config.routes.list}>Close</Link>
+      </nav>
+    </div>
+  )
+}
 
 SingleIdea.propTypes = {
-	title: PropTypes.string,
-	content: PropTypes.string,
-	sections: PropTypes.array,
-};
+  title: PropTypes.string,
+  content: PropTypes.string,
+  sections: PropTypes.array,
+}
 
-export default SingleIdea;
+export default SingleIdea
--- src/containers/idea-view-container.js
+++ src/containers/idea-view-container.js
@@ -1,56 +1,62 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import {setActiveIdea, unsetActiveIdea, updateActiveIdea} from '../actions/actions-active-idea';
-import { updateIdea } from '../actions/actions-ideas';
-import IdeaView from '../views/idea-view';
+import { setActiveIdea, unsetActiveIdea, updateActiveIdea } from '../actions/actions-active-idea'
+import { updateIdea } from '../actions/actions-ideas'
+import { toggleCompactIdeaView } from '../actions/actions-header'
+import IdeaView from '../views/idea-view'
 
 function mapStateToProps(state) {
-	return {
-		idea : state.activeIdea
-	}
+  return {
+    idea: state.activeIdea,
+    ideaCompactView: state.header.ideaCompactView,
+  }
 }
 
 const mapDispatchToProps = {
-	fetchIdea : setActiveIdea,
-	unsetActiveIdea,
-	updateActiveIdea,
-	updateIdea
-};
+  fetchIdea: setActiveIdea,
+  unsetActiveIdea,
+  updateActiveIdea,
+  updateIdea,
+  toggleCompactIdeaView,
+}
 
 
 class IdeaViewContainer extends Component {
-	static propTypes = {
-		idea: PropTypes.object		
-	};
+  static propTypes = {
+    idea: PropTypes.object
+  }
 
-	componentWillMount(){
-		this.props.fetchIdea(this.props.match.params.id);
-		this.updateIdeaSections = this.updateIdeaSections.bind(this);
-	}
+  componentWillMount() {
+    this.props.fetchIdea(this.props.match.params.id)
+    this.updateIdeaSections = this.updateIdeaSections.bind(this)
+  }
 
-	componentWillUnmount(){
-		this.props.unsetActiveIdea();
-	}
+  componentWillUnmount() {
+    this.props.unsetActiveIdea()
+  }
 
-	render(){
-		const {idea, match} = this.props;
-		return (
-			<div>
-				{idea && (
-					<IdeaView
-						idea={idea}
-						id={match.params.id}
-						updateIdeaSections={this.updateIdeaSections} />
-				)}
-			</div>
-		)
-	}
-	updateIdeaSections(sections){
-		this.props.updateActiveIdea({sections});
-		this.props.updateIdea(this.props.match.params.id, {sections});
-	}
+  render() {
+    const { idea, match, toggleCompactIdeaView, ideaCompactView } = this.props
+    return (
+      <div>
+        {idea && (
+          <IdeaView
+            idea={idea}
+            ideaCompactView={ideaCompactView}
+            id={match.params.id}
+            toggleCompactIdeaView={toggleCompactIdeaView}
+            updateIdeaSections={this.updateIdeaSections}/>
+        )}
+      </div>
+    )
+  }
+
+  updateIdeaSections(sections) {
+    this.props.updateActiveIdea({ sections })
+    this.props.updateIdea(this.props.match.params.id, { sections })
+  }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(IdeaViewContainer));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(IdeaViewContainer))
