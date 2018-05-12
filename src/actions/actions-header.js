0ac3b2d4b082b90bed239e282dd574ebd9b1d62f
oldirony <raffaele.abramini@gmail.com>
2018-05-12 13:23:30 +0100

    State
    - add 'ideaCompactView' concept in state

--- src/actions/actions-header.js
+++ src/actions/actions-header.js
@@ -1,8 +1,15 @@
 export const TOGGLE_FILTER_TAB = 'TOGGLE_FILTER_TAB';
+export const TOGGLE_COMPACT_IDEA_VIEW = 'TOGGLE_COMPACT_IDEA_VIEW';
 
 
 export const toggleFilterTab = ()=>{
 	return {
 		type: TOGGLE_FILTER_TAB
+	}
+}
+
+export const toggleCompactIdeaView = ()=>{
+	return {
+		type: TOGGLE_COMPACT_IDEA_VIEW
 	}
 }
--- src/reducers/reducer-header.js
+++ src/reducers/reducer-header.js
@@ -1,16 +1,19 @@
-import {TOGGLE_FILTER_TAB} from '../actions/actions-header';
+import { TOGGLE_FILTER_TAB, TOGGLE_COMPACT_IDEA_VIEW } from '../actions/actions-header'
 
 const initialState = {
-	filterTabOpen: false,
-	userTabOpen: false
+  filterTabOpen: false,
+  userTabOpen: false,
+  ideaCompactView: false,
 }
 
 
-export default function(state = initialState, action){
-	switch (action.type){
-		case TOGGLE_FILTER_TAB:
-			return {...state, filterTabOpen: !state.filterTabOpen}
-	}
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_FILTER_TAB:
+      return { ...state, filterTabOpen: !state.filterTabOpen }
+    case TOGGLE_COMPACT_IDEA_VIEW:
+      return { ...state, ideaCompactView: !state.ideaCompactView }
+  }
 
-	return state;
+  return state
 }
